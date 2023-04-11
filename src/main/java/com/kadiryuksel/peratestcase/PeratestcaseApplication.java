0eecd8721591295a9a13b1cb2917017edb985635
WastedHymn <kadir.yuksel@outlook.com.tr>
2023-04-12 02:54:05 +0300

    Initial commit

---
+++ src/main/java/com/kadiryuksel/peratestcase/PeratestcaseApplication.java
@@ -0,0 +1,13 @@
+package com.kadiryuksel.peratestcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeratestcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeratestcaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kadiryuksel/peratestcase/PeratestcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kadiryuksel.peratestcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeratestcaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
