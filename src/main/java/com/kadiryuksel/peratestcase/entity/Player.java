1a09a8a861ffdd9847567b8a052f283d9c64e331
WastedHymn <kadir.yuksel@outlook.com.tr>
2023-04-15 01:42:49 +0300

    Separate player name into first name and last name. Add data annotation and constructor annotations.

--- src/main/java/com/kadiryuksel/peratestcase/entity/Player.java
+++ src/main/java/com/kadiryuksel/peratestcase/entity/Player.java
@@ -2,19 +2,30 @@ package com.kadiryuksel.peratestcase.entity;
 
 import com.kadiryuksel.peratestcase.enums.Nationality;
 import com.kadiryuksel.peratestcase.enums.PlayerType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "player")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Player {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "player_name")
-    private String playerName;
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
 
     @Enumerated(EnumType.STRING)
     private Nationality nationality;
