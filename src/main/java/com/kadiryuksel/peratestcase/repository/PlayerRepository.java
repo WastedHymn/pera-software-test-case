b026f5b004ad773bf43196915edd2a9dd37fa475
WastedHymn <kadir.yuksel@outlook.com.tr>
2023-04-15 17:53:22 +0300

    Add new queries to the player repository.

--- src/main/java/com/kadiryuksel/peratestcase/repository/PlayerRepository.java
+++ src/main/java/com/kadiryuksel/peratestcase/repository/PlayerRepository.java
@@ -2,6 +2,7 @@ package com.kadiryuksel.peratestcase.repository;
 
 import com.kadiryuksel.peratestcase.entity.Player;
 import com.kadiryuksel.peratestcase.enums.Nationality;
+import com.kadiryuksel.peratestcase.enums.PlayerType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,6 +12,10 @@ public interface PlayerRepository extends JpaRepository<Player, Long> {
     List<Player> getPlayersByTeamId(long teamId);
 
     int countByNationalityAndTeamId(Nationality nationality, long teamId);
+
+    int countByPlayerTypeAndTeamId(PlayerType type, long teamId);
+
+    int countByTeamId(long teamId);
 
     void deleteAllByTeamId(long teamId);
 
