637ebfa72836248997cfabae37b88b4f215476be
WastedHymn <kadir.yuksel@outlook.com.tr>
2023-04-15 18:56:22 +0300

    Change TeamNotFoundException class name to NotFoundException and change the class constructor.

---
+++ src/main/java/com/kadiryuksel/peratestcase/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.kadiryuksel.peratestcase.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
