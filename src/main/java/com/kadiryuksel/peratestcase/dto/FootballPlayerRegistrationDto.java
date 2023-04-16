63eb79fc84f0a849b0ae1a9b84dd3c1443a4d458
WastedHymn <kadir.yuksel@outlook.com.tr>
2023-04-16 22:23:00 +0300

    Add builder, noargsconstructor and allargsconstructor annotations.

--- src/main/java/com/kadiryuksel/peratestcase/dto/FootballPlayerRegistrationDto.java
+++ src/main/java/com/kadiryuksel/peratestcase/dto/FootballPlayerRegistrationDto.java
@@ -3,12 +3,18 @@ package com.kadiryuksel.peratestcase.dto;
 import com.kadiryuksel.peratestcase.enums.Nationality;
 import com.kadiryuksel.peratestcase.enums.PlayerType;
 import com.kadiryuksel.peratestcase.util.ConstantMessages;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.PositiveOrZero;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
+@Builder
 public class FootballPlayerRegistrationDto {
     @NotBlank(message = "Enter a valid first name.")
     private String firstName;
