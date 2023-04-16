67130cb6eb56f70f65f0b3bcb91e5b2c42c44230
WastedHymn <kadir.yuksel@outlook.com.tr>
2023-04-16 15:48:17 +0300

    Change the constructor's parameter.

--- src/main/java/com/kadiryuksel/peratestcase/exception/TeamAlreadyExistsException.java
+++ src/main/java/com/kadiryuksel/peratestcase/exception/TeamAlreadyExistsException.java
@@ -2,7 +2,7 @@ package com.kadiryuksel.peratestcase.exception;
 
 public class TeamAlreadyExistsException extends RuntimeException {
 
-    public TeamAlreadyExistsException(String teamName) {
-        super(String.format("%s already exists.", teamName));
+    public TeamAlreadyExistsException(String message) {
+        super(message);
     }
 }
